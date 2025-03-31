cc2fc92eadd6ab08fb40f30c2a93c5f69598712e
Nightwatcher85 <gregoryjepsen@gmail.com>
2025-03-30 20:58:39 -0400

    Customized redix UI theme

--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 
 const geistSans = Geist({
@@ -32,6 +32,7 @@ export default function RootLayout({
         <Theme> 
         <NavBar />
         <main className = 'p-5'>{children}</main>
+        <ThemePanel />
         </Theme>
       </body>
     </html>
