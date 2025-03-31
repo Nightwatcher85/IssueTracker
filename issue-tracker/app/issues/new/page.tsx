3ac5a9b6c653eee6c213e2719fb8b744e43fcd25
Nightwatcher85 <gregoryjepsen@gmail.com>
2025-03-31 00:25:31 -0400

    Moved handling outsite of the rendering

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -29,6 +29,17 @@ const NewIssuePage = () => {
   const [error,setError] = useState('');
   const [isSubmitting, setSubmitting] = useState(false);
 
+const onSubmit =handleSubmit(async (data) => {
+  try {
+    setSubmitting(true);
+    await axios.post('/api/issues', data);
+    router.push('/issues');
+  } catch (error) {
+    setSubmitting(false);
+    setError('An unexpected error occured.')
+  }
+})
+
   return (
     <div  className='max-w-xl space-y-3' >
       {error && <Callout.Root color="red" className='mb-5'>
@@ -36,17 +47,7 @@ const NewIssuePage = () => {
         </Callout.Root>}
     <form 
   
-      onSubmit={
-        handleSubmit(async (data) => {
-        try {
-          setSubmitting(true);
-          await axios.post('/api/issues', data);
-          router.push('/issues');
-        } catch (error) {
-          setSubmitting(false);
-          setError('An unexpected error occured.')
-        }
-      })}>
+      onSubmit={onSubmit}>
 
       <div>
         <Label htmlFor="title">Title</Label>
